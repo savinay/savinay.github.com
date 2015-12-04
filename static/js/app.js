eceac9e4fc856b66df5b295346d10cc25f20434b
Savinay Narendra <savinay.90@gmail.com>
2015-12-04 10:35:30 +0530

    added Email functionality

--- static/js/app.js
+++ static/js/app.js
@@ -11,23 +11,51 @@ $(function() {
             alert("Please enter your message");
         }
         if (email && msg) {
-            var data = {
+            var data_body = {
                 name: name,
                 email: email,
                 phone: phone,
                 msg: msg
             }
+
+            message = msg + 'Name and Phone no: are ' + name + ', ' + phone
+
             $.ajax({
-                type: "POST",
-                url: '/sendEmail',
-                data: data,
-                success: function () {
-                    console.log("success");
-                },
-                error: function () {
-                    console.log("error");
+                type: 'POST',
+                url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+                data: {
+                'key': 'o2_eZLXnlK1JeXvabw546g',
+                'message': {
+                    'from_email': email,
+                    'to': [
+                        {
+                            'email': 'savinay.90@gmail.com',
+                            'name': 'Savinay Narendra',
+                            'type': 'to'
+                        }
+                    ],
+                    'autotext': 'true',
+                    'subject': 'New Email from Personal Website',
+                    'html': message
                 }
-            });
+              }
+             }).done(function(response) {
+               console.log(response); // if you're into that sorta thing
+             });
+
+
+
+            // $.ajax({
+            //     type: "POST",
+            //     url: '/sendEmail',
+            //     data: data,
+            //     success: function () {
+            //         console.log("success");
+            //     },
+            //     error: function () {
+            //         console.log("error");
+            //     }
+            // });
         }
     })
 })
